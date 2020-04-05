768b3b2cfa54e4d68c532016ab8a6885d51470c2
zhiqian.liu <zhiqian.liu@baifendian.com>
2020-04-05 22:49:05 +0800

    fix update demo

--- demo/ctbuild.config.js
+++ demo/ctbuild.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
   getConfig({ curModule }) {
-    curModule.resolve.alias = {
-      react: path.join(__dirname, 'node_modules/react'),
-      'react-dom': path.join(__dirname, 'node_modules/react-dom'),
-    };
+    // curModule.resolve.alias = {
+    //   react: path.join(__dirname, 'node_modules/react'),
+    //   'react-dom': path.join(__dirname, 'node_modules/react-dom'),
+    // };
 
     // curModule.resolve.alias = {
     //   '@ctsj/router': path.resolve(__dirname, 'src/ctsj-router'),
